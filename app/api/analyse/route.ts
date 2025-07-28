ccb90e5485c5efdeb2a7df3731f47689a3cb4962
ALEX S <165883029+Alex-S1by@users.noreply.github.com>
2025-07-28 20:01:34 +0530

    Update route.ts

--- app/api/analyse/route.ts
+++ app/api/analyse/route.ts
@@ -4,12 +4,42 @@ import {  NextRequest, NextResponse } from 'next/server';
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export const POST = async (req: NextRequest) => {
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  
     try{
+      const body = await req.json();
 
-        const body = await req.json();
+        if (typeof body === 'string') {
+          const prompt = `here is the user query and questions. answer the user query accordingly ${body}`
         
+           
+const result = await model.generateContent(prompt);
+const responseText = result.response.text();
+
+
+
+
+
+
+const lines = responseText.split('\n');
+let res="";
+lines.forEach(line => {
+  
+    const message = line.replace('*','').trim();
+    
+      res =res+""+message
+   
+       
+    
+     
+   
+  })
+
+console.log("string");
+return NextResponse.json({res}); 
         
-        
+        }
         const extractedTexts: { [key: string]: string } = {};
         body.forEach((item: { text: string }, index: number) => {
           extractedTexts[`set${index + 1}`] = item.text;
@@ -19,14 +49,12 @@ export const POST = async (req: NextRequest) => {
         
         const combinedTexts = Object.values(extractedTexts).join('\n');
         
-        const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-        
+      
         
         
 
 
-const prompt = `Here are the previous years' question papers:\n${combinedTexts}\n\nPlease find repeated questions[exact question with count not other content],repeated topics from the above question paper sets.`;
+const prompt = `Here are the previous years' question papers:\n${combinedTexts}\n\nPlease find repeated questions [detailed questions with year and number],repeated topics from the above question paper sets.`;
 
 
 
